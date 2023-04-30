55477e575360bb7f4310ba62874f227884398bc7
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-04-30 10:35:44 +0200

    Initial scaffolding

---
+++ BattlePlan/BattlePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattlePlanApp.swift
+//  BattlePlan
+//
+//  Created by Michael Bisgaard Olesen on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BattlePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
